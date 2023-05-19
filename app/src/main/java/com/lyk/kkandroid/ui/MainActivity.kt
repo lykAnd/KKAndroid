ccb50a1c9f9923d5c5f8e271ee8961207044b11c
linyingkun <332329853@qq.com>
2023-05-19 18:00:50 +0800

    练习
    Signed-off-by: linyingkun <332329853@qq.com>

--- app/src/main/java/com/lyk/kkandroid/ui/MainActivity.kt
+++ app/src/main/java/com/lyk/kkandroid/ui/MainActivity.kt
@@ -1,46 +1,37 @@
-package com.lyk.kkandroid
+package com.lyk.kkandroid.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lyk.kkandroid.ui.test.PositionItem
 import com.lyk.kkandroid.ui.theme.KKAndroidTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val names: List<String> = ArrayList<String>().apply {
+        add("Android")
+        add("Java")
+        add("Kotlin")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KKAndroidTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                PositionItem()
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     KKAndroidTheme {
-        Greeting("Android")
+        PositionItem()
     }
 }
